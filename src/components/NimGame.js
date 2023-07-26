08061e5a21839666cd715204d440580cf906f09b
Michael Perkins <mperkins1995@gmail.com>
2023-07-26 00:22:43 -0400

    fixing turn based moves

--- src/components/NimGame.js
+++ src/components/NimGame.js
@@ -26,14 +26,30 @@ const NimGame = () => {
     setSelectedSticks(sticksCount);
   };
 
+  const isCurrentPlayer = () => {
+    if (gameData) {
+      if (currentPlayer === 1) {
+        return gameData.player_1_ID === auth.currentUser.uid;
+      }
+      return gameData.player_2_ID === auth.currentUser.uid;
+    }
+    console.log("Game has not started yet!");
+    return false;
+  };
+
   const handlePlay = () => {
+    if (!isCurrentPlayer()) {
+      console.log("Not your turn!");
+      return;
+    }
     if (selectedPile !== null && selectedSticks !== null) {
       const gameRef = ref(database, `games/${gameId}`);
       get(gameRef).then((snapshot) => {
         const gameData = snapshot.val();
         const piles = [...gameData.piles];
         piles[selectedPile] -= selectedSticks;
-        const currentPlayer = gameData.currentPlayer === 1 ? 2 : 1;
+        const currentPlayer =
+          gameData.currentPlayer === "player_1" ? "player_2" : "player_1";
         set(gameRef, { ...gameData, piles, currentPlayer });
       });
 
@@ -41,7 +57,6 @@ const NimGame = () => {
       setSelectedSticks(null);
     }
   };
-
   const checkGameStatus = () => {
     if (gameData) {
       const isGameOver = gameData.piles.every((pile) => pile === 0);
@@ -58,56 +73,56 @@ const NimGame = () => {
   };
 
   return (
-    <div>
-      <h1>Nim Game</h1>
-      <div>
+    <div className="min-h-screen bg-gray-100 py-8">
+      <div className="max-w-md mx-auto px-4">
+        <h1 className="text-4xl font-bold mb-4">Nim Game</h1>
         {gameData && (
           <div>
-            <h2>Game ID: {gameId}</h2>
-            <h3>Current Player: {currentPlayer}</h3>
-            {selectedPile !== null && (
-              <div>
-                <h3>Selected Pile: {selectedPile + 1}</h3>
-                {selectedSticks !== null && (
-                  <h3>Selected Sticks: {selectedSticks}</h3>
-                )}
-              </div>
-            )}
-            <h3>Piles:</h3>
-            {gameData.piles.map((pile, pileIndex) => (
-              <div key={pileIndex}>
-                <div>
-                  {Array(pile)
-                    .fill()
-                    .map((_, sticksIndex) => (
-                      <button
-                        key={sticksIndex}
-                        onClick={() => handleSticksClick(pileIndex, sticksIndex + 1)}
-                        className={
-                          selectedPile === pileIndex &&
-                          selectedSticks === sticksIndex + 1
-                            ? "selected"
-                            : ""
-                        }
-                      >
-                        {sticksIndex + 1}
-                      </button>
-                    ))}
+            {/* ... (rest of the game information display) */}
+            <div className="mt-4">
+              <h3 className="font-bold mb-2">Piles:</h3>
+              {gameData.piles.map((pile, pileIndex) => (
+                <div key={pileIndex} className="mb-4">
+                  <div className="flex items-center">
+                    {Array(pile)
+                      .fill()
+                      .map((_, sticksIndex) => (
+                        <button
+                          key={sticksIndex}
+                          onClick={() =>
+                            handleSticksClick(pileIndex, sticksIndex + 1)
+                          }
+                          className={`${
+                            selectedPile === pileIndex &&
+                            selectedSticks === sticksIndex + 1
+                              ? "bg-blue-500 text-white"
+                              : "bg-gray-300 text-gray-700"
+                          } py-2 px-4 rounded-lg mr-2`}
+                        >
+                          {sticksIndex + 1}
+                        </button>
+                      ))}
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
             {checkGameStatus() && (
-              <div>
-                <h3>Game Over!</h3>
+              <div className="mt-4">
+                <h3 className="font-bold">Game Over!</h3>
                 {getWinner() ? (
-                  <p>Player {getWinner()} wins!</p>
+                  <p className="mt-2">Player {getWinner()} wins!</p>
                 ) : (
-                  <p>It's a draw!</p>
+                  <p className="mt-2">It's a draw!</p>
                 )}
               </div>
             )}
             {!checkGameStatus() && (
-              <button onClick={handlePlay}>Play</button>
+              <button
+                onClick={handlePlay}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg mt-4"
+              >
+                Play
+              </button>
             )}
           </div>
         )}
